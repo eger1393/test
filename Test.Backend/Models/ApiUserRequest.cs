6504c3a510a14f6985f235d5e5853370f999a898
eger1393 <eger1393@gmail.com>
2021-07-13 09:25:44 +0300

    fix(api): Удалил из ApiUserRequest лишнее анатирование
    которое попало туда в следствии копипаста

--- Test.Backend/Models/ApiUserRequest.cs
+++ Test.Backend/Models/ApiUserRequest.cs
@@ -7,13 +7,9 @@ namespace Test.Backend.Models
     public class ApiUserRequest
     {
         [Required]
-        [DataType(DataType.Date)]
-        [Column(TypeName = "Date")]
         public DateTime RegistrationDate { get; set; }
 
         [Required]
-        [DataType(DataType.Date)]
-        [Column(TypeName = "Date")]
         public DateTime LastActivityDate { get; set; }
     }
 }
