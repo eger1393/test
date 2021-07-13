1abf6fd9b0ee18b83e05c432058b0ae33bc1a066
eger1393 <eger1393@gmail.com>
2021-07-13 10:11:19 +0300

    fix: Перенес папку Test.Services в папку с проектом
    До этого она была на дерикторию выше

---
+++ Test.Services/Models/LifeSpan.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.Services.Models
+{
+    public class LifeSpan
+    {
+        /// <summary>
+        /// Время жизни пользователя в днях
+        /// </summary>
+        [Required]
+        public ushort LifeSpanDays { get; set; }
+
+        /// <summary>
+        /// Кол-во таких пользователей
+        /// </summary>
+        [Required]
+        public int Count { get; set; }
+    }
+}
---
+++ Test.Services/Services/AnalyticService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Data.Repository;
+using Test.Services.Models;
+
+namespace Test.Services.Services
+{
+    public interface IAnalyticService
+    {
+        /// <summary>
+        /// Вычисление повторяющеегося удержания
+        /// Расчитывается по формуле Количество пользователей, зашедших в день N или позже / Количество пользователей, установивших приложение N дней назад * 100%
+        /// </summary>
+        /// <param name="days"></param>
+        /// <returns></returns>
+        public Task<double> CalculateRollingRetentionAsync(ushort days);
+
+        /// <summary>
+        /// Вычисление распределения времени жизни пользователей
+        /// </summary>
+        /// <returns></returns>
+        public Task<IEnumerable<LifeSpan>> GetLifeSpanDistribution();
+    }
+
+    public class AnalyticService : IAnalyticService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AnalyticService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<double> CalculateRollingRetentionAsync(ushort days)
+        {
+            var activeUsersCount = _userRepository.GetCountActiveUsersAfter(days);
+            var registeredUsersCount = _userRepository.GetCountUsersWhoRegisteredEarlierThan(DateTime.Today.AddDays(-days));
+            await Task.WhenAll(activeUsersCount, registeredUsersCount);
+            var rollingRetention = (double)activeUsersCount.Result / registeredUsersCount.Result * 100;
+            return rollingRetention;
+        }
+
+        public async Task<IEnumerable<LifeSpan>> GetLifeSpanDistribution()
+        {
+            var users = await _userRepository.GetAllAsync();
+            var lifeSpanDays = users.Select(x => x.LifeSpanDays).ToList();
+            var result = lifeSpanDays.Distinct()
+                .OrderBy(x => x)
+                .Select(x => new LifeSpan()
+                {
+                    LifeSpanDays = x,
+                    Count = lifeSpanDays.Count(lifeSpan => lifeSpan == x)
+                });
+            return result;
+        }
+    }
+}
